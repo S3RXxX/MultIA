9a517fa99900b74d345fbd7102bf2948824f9425
Sergi Guimerà Roig <sergiguimeraroig@gmail.com>
2024-06-29 13:08:38 +0200

    visual skeleton

--- src/Flask_try.py
+++ src/Flask_try.py
@@ -1,7 +1,12 @@
-import flask
+from flask import Flask, render_template, url_for, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 def do_all():
     print("Hello from the other side")
 
 if __name__=="__main__":
-    do_all()
+    app.run(debug=True)
