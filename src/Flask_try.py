576e6ce027abb56e34f8cb4e90f7db044a95f99b
Sergi Guimerà Roig <sergiguimeraroig@gmail.com>
2024-07-05 13:25:59 +0200

    Delete done ig, time for Update

--- src/Flask_try.py
+++ src/Flask_try.py
@@ -1,6 +1,7 @@
 import ollama
 from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
+messages = []
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -12,7 +13,7 @@ def index():
             return "There was an issue while thinking for a response"
         return redirect('/')
     else:
-
+        # afegir missatges a BD
         return render_template("index.html", messages=messages)
 
 def LLM_chat(text):
@@ -24,9 +25,26 @@ def LLM_chat(text):
 
     response = ollama.chat(model='llama3:instruct', messages=messages)
     messages.append(response['message'])
+    
+def add_to_db(message):
+    pass
+
+@app.route('/delete/<int:n>')
+def delete(n):
+    n -= 1 ## loop.index starts w 1
+    global messages
+    try:
+        #### borrar a BD
+        if n == 0:
+            messages = []
+        else:
+            messages = messages[:n]
+        return redirect('/')
+    except:
+        return "There was an error while erasing your messages"
+    
+
 
 
 if __name__=="__main__":
-    messages = []
-
     app.run(debug=True)
